b6a8a84a467ac5d974b6223972609d1f9b75f64f
32Fire32 <nicola.gianfelici@hotmail.it>
2023-01-30 15:21:07 +0100

    definisce la relazione tra i model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,10 @@ class Project extends Model
     use HasFactory;
     protected $guarded = ['slug'];
 
+    use HasFactory;
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
 }
